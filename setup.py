53c5c48f847ff703f4e087a0ce87d71bc64f3a62
Rex <rexgreenway@gmail.com>
2021-09-02 15:44:27 +0100

    Package SetUp

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="alexdata",
+    url = "https://github.com/RexGreenway/",
+    packages=["alexdata"],
+    version="1.0.0",
+    description="...",
+    author="Thomas Rex Greenway",
+)
